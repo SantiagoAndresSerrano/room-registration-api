33fcaf11effda7e0b028d931c2dfecdd9b672d98
Jhonny Esneider Guarin Chavez <55068431+GenesisDaniela@users.noreply.github.com>
2022-10-20 10:22:39 -0500

    Procfile fixed

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from app import create_app
 import py_eureka_client.eureka_client as eureka_client
 
-eureka_client.init(eureka_server="https://eureka-service-ufps.herokuapp.com/eureka",
-                   app_name="registermicroservice")
+# eureka_client.init(eureka_server="https://eureka-service-ufps.herokuapp.com/eureka",
+#                    app_name="registermicroservice")
 app = create_app()
 
 if __name__ == "__main__":
