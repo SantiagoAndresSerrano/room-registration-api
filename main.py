47f46e45ebdd54183bbac62ff33469961eb5e57b
Santiago Serrano <62350078+SantiagoAndresSerrano@users.noreply.github.com>
2022-12-06 12:35:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from app import create_app
 import flask
-import py_eureka_client.eureka_client as eureka_client
 
-eureka_client.init(eureka_server="https://eureka-service-ufps.herokuapp.com/eureka",
-                   app_name="registermicroservice")
 app = create_app()
 
 if __name__ == "__main__":
