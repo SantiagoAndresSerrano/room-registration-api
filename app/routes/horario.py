bc233fbc73c878e7348035fcb81099abed05f510
Génesis Daniela Vargas Jáuregui <genesisdvargas4@gmail.com>
2022-12-05 21:23:50 -0500

    Update horario.py

--- app/routes/horario.py
+++ app/routes/horario.py
@@ -193,3 +193,36 @@ def consultarHorarioSalon(id_salon, dia):
     except NoResultFound:
         return "no detail with this schedule can be found", status.HTTP_401_UNAUTHORIZED
     return horarios_schema.dump(detalleHorarioFound), status.HTTP_200_OK
+
+@horarios.route("/roomregister/horario/fecha/<string:id_salon>/<string:fecha>", methods=["GET"])
+@cross_origin()
+def consultarHorarioSalonFecha(id_salon, fecha):
+    """Retorna el horario de un salón determinado día
+      ---
+      tags:
+        - Horario
+      parameters:
+        - name: id_salon
+          in: path
+          type: string
+          required: true
+          description: Identifier salon, example (SA401)
+        - name: fecha
+          in: path
+          type: string
+          format: date-time
+          required: true
+          description: Identifier dia, example (2022-08-15 10:10:00)
+      responses:
+        200:
+          description: list horario
+          schema:
+            $ref: '#/definitions/Horario'
+      """
+    try:
+        dia = datetime.strptime(fecha, "%Y-%m-%dT%H:%M:%S").weekday()
+        print(dia)
+        detalleHorarioFound = Horario.query.join(DetalleHorario).join(GrupoMateria).join(Salon).filter(and_(Salon.id_salon== id_salon, GrupoMateria.id_grup_mat==Salon.grupo_materia, DetalleHorario.grupo_materia == GrupoMateria.id_grup_mat, Horario.id_horario == DetalleHorario.id_det_hor, Horario.dia == dia))
+    except NoResultFound:
+        return "no detail with this schedule can be found", status.HTTP_401_UNAUTHORIZED
+    return horarios_schema.dump(detalleHorarioFound), status.HTTP_200_OK
